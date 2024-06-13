3c62c37e14a3a01664967b0ce1feaef7ff11dd3f
FahdAzouz <fahdstudies00@gmail.com>
2024-06-13 21:36:11 +0100

    task 0

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+'''Finding the minimum number of operations to devide a number'''
+
+
+def minOperations(n):
+    '''calculates the fewest number of
+    operations needed to result in exactly n H
+    characters in this file.
+    Returns:
+        Integer : if n is impossible to achieve, return 0
+    '''
+    if n <= 1:
+        return 0
+
+    operations = 0
+    factor = 2
+
+    while factor * factor <= n:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+
+    if n > 1:
+        operations += n
+
+    return operations
+
