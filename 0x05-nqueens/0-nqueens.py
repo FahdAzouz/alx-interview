4fa43c5a8c2b547f9162c74b914db75132558c6e
FahdAzouz <fahdstudies00@gmail.com>
2024-07-04 22:09:17 +0100

    sttyle

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
 import sys
 
+
 def is_safe(board, row, col):
     # Check all columns to the left of the current position
     for i in range(col):
         # Check if there's a queen in the same row
         if board[i] == row:
             return False
-        
+
         # Check upper-left diagonal
         # This works for any distance because the difference is always the same
         if board[i] - i == row - col:
             return False
-        
+
         # Check lower-left diagonal
         # This works for any distance because the sum is always the same
         if board[i] + i == row + col:
             return False
-    
+
     # If we've checked all positions and found no threats, it's safe
     return True
+
 
 def solve_nqueens(N):
     def backtrack(col):
@@ -33,6 +35,7 @@ def solve_nqueens(N):
 
     board = [-1] * N
     backtrack(0)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
