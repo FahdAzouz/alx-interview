5a617bdf9ce0896d0dc7e3067f108c3e99eb8682
FahdAzouz <fahdstudies00@gmail.com>
2024-07-12 01:07:04 +0100

    task 0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,52 +2,16 @@
 
 const request = require('request');
 
-const movieId = process.argv[2];
-const people = [];
-const films = [];
-const filmEndPoint = 'https://swapi-api.hbtn.io/api/films/' + movieId;
-
-const requestCharacters = async () => {
-  await new Promise(resolve => request(filmEndPoint, (err, res, body) => {
-    if (err || res.statusCode !== 200) {
-      console.error('Error: ', err, '| StatusCode: ', res.statusCode);
-    } else {
-      jbody = JSON.parse(body);
-      people = jbody.characters;
-      resolve();
-    }
-  }));
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
+});
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
+  });
 };
-
-const requestFilms = async () => {
-  if (people > 0) {
-    for (const p of people) {
-      await new Promise(resolve => request(p, (err, res, body) => {
-        if (err || res.statusCode !== 200) {
-          console.error('Error: ', err, '| StatusCode: ', res.statusCode);
-        } else {
-          const jbody = JSON.parse(body);
-          films.push(jbody.name);
-          resolve();
-        }
-      }));
-    }
-  } else {
-    console.error('Error : Got no characters to look up');
-  }
-};
-
-const getCharNames = async () => {
-  await requestCharacters;
-  await requestFilms;
-
-  for (const f of films) {
-    if (f === films[films.length - 1]) {
-      process.stdout.write(f);
-    } else {
-      process.stdout.write(f + '\n');
-    }
-  }
-};
-
-getCharNames();
